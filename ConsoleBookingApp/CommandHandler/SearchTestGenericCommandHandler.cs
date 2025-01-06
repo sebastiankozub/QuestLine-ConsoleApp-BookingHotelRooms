2d8a30346893c9795b1de6c900d23a33856a3a25
skozub <sebastiankozub@o2.com>
2025-01-06 23:59:06 +0100

    update last changes before branching

---
+++ ConsoleBookingApp/CommandHandler/SearchTestGenericCommandHandler.cs
@@ -0,0 +1,66 @@
+﻿using BookingApp.Service;
+using System.Text;
+
+namespace ConsoleBookingApp.CommandHandler;
+
+public class SearchTestGenericCommandHandler(IRoomAvailabilityService roomAvailabilityService) : CommandHandler("NewSearch")
+{
+    private readonly IRoomAvailabilityService _roomAvailabilityService = roomAvailabilityService;
+
+    public async override Task<ICommandHandlerResult> HandleAsync(string[] parameters)
+    {
+        try
+        {
+            var (hotelId, availabitlityPeriod, roomType) = SearchCommandValidator.Parse(parameters);
+            SearchCommandValidator.Validate(hotelId, availabitlityPeriod, roomType);
+
+            var roomAvailabilities = await _roomAvailabilityService
+                .GetRoomAvailabilityByRoomType(hotelId, availabitlityPeriod, roomType, true);
+
+            var outputBuilder = new StringBuilder();
+            var roomAvailabilitiesCount = roomAvailabilities.Count();
+            var emptyLineAdded = false;
+
+            for (int i = 0; i < roomAvailabilitiesCount;  )
+            {
+                var roomAvailability = roomAvailabilities.ElementAt(i);
+
+                if (roomAvailability.RoomAvailabilityCount > 0)
+                {
+                    if (roomAvailability.SameCountPeriod == 1)
+                    {
+                        outputBuilder.AppendLine(
+                            "(" + $"{roomAvailability.Day:yyyyMMdd}" + ","
+                            + $"{roomAvailability.RoomAvailabilityCount}" + "),");
+                        i += 1;
+                    }
+                    else
+                    {
+                        outputBuilder.AppendLine(
+                            "(" + $"{roomAvailability.Day:yyyyMMdd} - {roomAvailability.Day.AddDays((int)roomAvailability.SameCountPeriod - 1):yyyyMMdd}"
+                            + "," + $"{roomAvailability.RoomAvailabilityCount}" + "),");
+                        i += (int)roomAvailability.SameCountPeriod;
+                    }
+                    emptyLineAdded = false;
+                }
+                else 
+                { 
+                    if (!emptyLineAdded) 
+                    { 
+                        outputBuilder.AppendLine("");
+                        emptyLineAdded = true; 
+                    }
+                    i += 1;
+                }
+            }
+
+            return new SearchCommandHandlerResult { Success = true, ResultData = outputBuilder.ToString() };
+        }
+        catch (Exception ex) when (ex is SearchCommandHandlerParseException 
+            || ex is SearchCommandHandlerValidateException 
+            || ex is RoomAvailabilityServiceException)
+        {
+            return HandleExceptionMessage<SearchCommandHandlerResult>(ex);
+        }
+    }
+}
